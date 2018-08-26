ef1c74168c2682df2af6bac822dad72e09f42d3d
JEIEL MARTINS BENEDITO <jeiel.benedito@sp01.local>
2018-08-26 12:57:03 -0300

    bot

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroProdutoComponent } from './produtos/cadastro-produto/cadastro-produto.component';
 import {CalculaprecoComponent} from './produtos/calculapreco/calculapreco.component';
+import {OrcamentoComponent} from './produtos/orcamento/orcamento.component';
 import { PageComponent } from './home/page/page.component';
+import { ServicoComponent } from './produtos/servico/servico.component';
 
 const routes: Routes = [
   { path: 'cadastro-produtos', component: CadastroProdutoComponent },
   { path: 'calcula-preco', component: CalculaprecoComponent },
+  { path: 'orcamento', component: OrcamentoComponent },
+  { path: 'servicos', component: ServicoComponent },
   { path: 'home', component: PageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
 import { CalculaprecoComponent } from './calculapreco/calculapreco.component';
+import { OrcamentoComponent } from './orcamento/orcamento.component';
+import { ServicoComponent } from './servico/servico.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [CadastroProdutoComponent, CalculaprecoComponent],
+  declarations: [CadastroProdutoComponent, CalculaprecoComponent, OrcamentoComponent, ServicoComponent],
   exports: [CadastroProdutoComponent]
 })
 export class ProdutosModule { }
